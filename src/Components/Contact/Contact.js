0e85e0526f0942144e7d6156f4a1074077184a5e
HeberSanzS <123579177+HeberSanzS@users.noreply.github.com>
2023-03-21 06:26:18 -0600

    Contact c7modal

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,77 +3,106 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { Height } from '@mui/icons-material';
+
 
 function Contact() {
     return (
 
         <Box
-            component="form"
-            sx={{
+            component="form" // elemento raiz formulario HTML
+            sx={{  //Propiedad personalizada en el componente para agregar estilos personalizados utilizando "styled-system".
                 display: 'flex',
-                flexDirection: 'column',
+                flexDirection: 'column', //orientacion de los elementos
                 alignItems: 'center',
                 justifyContent: 'center',
                 textAlign: 'center',
                 fontFamily: 'monospace',
                 width: '100%',
-                maxWidth: '400px', // Set a maximum width for the form container
-                margin: '0 auto', // Center the container horizontally
+                maxWidth: '400px', 
+                margin: '0 auto', 
             }}
-            noValidate
-            autoComplete="off"
+            noValidate //Propiedad para que Este elemento no sea validado por el Navegador
+            autoComplete="off" //Autocompletado desactivado
         >
             <div>
-            <Typography variant="h5" mb={3} sx={{ fontFamily: "Lato, sans-serif", color: "#276678", fontSize: { xs: '2.5rem', sm: '3rem' }, marginTop: '3rem' }}>
+            <Typography //Bloque de texto con estilo Personalizado
+            variant="h5" // tipo de fuente o estilo que se va a utilizar para el texto.
+            mb={3} // margen inferior 3 unidades
+            sx={{ fontFamily: "Lato, sans-serif", 
+            color: "#276678", 
+            fontSize: { xs: '2.5rem', 
+            sm: '3rem' }, 
+            marginTop: '3rem' }}>
             Contáctanos
           </Typography>
-                <div style={{ marginBottom: "3rem", fontSize: "1.5rem", fontFamily: "Lato, sans-serif"}}>
+                <div style={{ marginBottom: "3rem",
+                 fontSize: "1.5rem",
+                  fontFamily: "Lato, sans-serif"}}>
                     Comparte tus comentarios o sugerencias al equipo de JavaWizards.
                 </div>
 
 
                 <div>
-                    <TextField
-                        required
-                        id="outlined-basic"
-                        label="Nombre"
+                    <TextField //Bloque de texto con estilo Personalizado
+                        required //Indica que el campo de entrada es obligatorio y debe ser completado por el usuario.
+                        id="outlined-basic" //Id para el campo de entrada
+                        label="Nombre" 
                         defaultValue=""
-                        variant="outlined"
-                        style={{ marginBottom: "16px", width: "500px" }}
+                        variant="outlined" //Variante de estilo del campo de entrada.
+                        style= //Agregar estilos personalizados al componente 
+                        {{ 
+                            marginBottom: "16px",
+                            width: "500px"
+                         }}
                     />
 
-                    <TextField
+                    <TextField //Bloque de texto con estilo Personalizado
                         required
                         id="outlined-basic"
                         label="E-mail"
                         defaultValue=""
                         variant="outlined"
-                        style={{ marginBottom: "16px", marginTop: "16px", width: "500px" }}
+                        style=
+                        {{
+                             marginBottom: "16px", 
+                             marginTop: "16px", 
+                             width: "500px"
+                         }}
                     />
 
-                    <TextField
+                    <TextField //Bloque de texto con estilo Personalizado
                         required
                         id="outlined-basic"
                         label="Asunto"
                         defaultValue=""
                         variant="outlined"
-                        style={{ marginBottom: "16px", marginTop: "16px", width: "500px" }}
+                        style={{ marginBottom: "16px", 
+                        marginTop: "16px", 
+                        width: "500px" }}
                     />
                 </div>
                 <div>
-                    <TextField id="filled-basic" label="Comentarios" variant="filled" style={{ marginBottom: "16px", marginTop: "16px", width: "500px"}}  />
+                    <TextField //Bloque de texto con estilo Personalizado
+                    id="filled-basic" 
+                    label="Comentarios" 
+                    variant="filled" //Propiedad usada el componentes Material UI para un estilo de fondo "relleno" en un componente de formulario,
+                    style={{ 
+                        marginBottom: "16px",
+                     marginTop: "16px",
+                      width: "500px"
+                      }}  />
                 </div>
 
-                <Stack spacing={1} direction="row" sx={{ justifyContent: 'center' }}>
+                <Stack //Componente MUI que se utiliza para apilar elementos vertical u horizontalmente.
+                spacing={1}  //Espacio entre los elementos apilados. 
+                direction="row" //Dirección en la que se apilan los elementos en ese caso fila.
+                sx={{  //Estilos personalizados al componente
+                    justifyContent: 'center' 
+                    }}>
 
-                    <Button variant="contained" sx={{ margin: '16px', width: '250px', height: '50px', fontFamily: 'monospace', marginTop: "50px" }}>
-                        Enviar
-                    </Button>
-
+                    
                 </Stack>
-
+                
             </div>
         </Box>
 
